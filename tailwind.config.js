275b0c5462c07b63b78264bf19bf424d614f8053
osmanelmaa <osmanalielma4616@gmail.com>
2024-03-11 16:49:36 +0300

    header design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    container:{
+      center: true,
+      padding: '2rem',
+      
+    },
+
+    extend: 
+    {
+      fontFamily: {
+        open: ['Open Sans', 'sans-serif'],
+        gemunu: ['Gemunu Libre', 'sans-serif']
+      },
+
+      colors: {
+        'my-red': '#BC1A45',
+        'my-melon': '#FFD369',
+        'my-grey': '#DDDDDD',
+        'my-white': '#F7F7F7',
+        
+      },
+    },
   },
   plugins: [],
 }
